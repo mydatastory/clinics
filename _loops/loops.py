567f9b8f150da2be161e8868afd5768509213a28
Daniel Maxwell <dsm_direct@yahoo.com>
2018-07-20 13:19:39 -0400

    Edits (07.20.18).

--- _loops/loops.py
+++ _loops/loops.py
@@ -2,8 +2,8 @@
 """
 Spyder Editor
 
-This is a temporary script file.
-https://jakevdp.github.io/PythonDataScienceHandbook/04.01-simple-line-plots.html
+The code sections in this file are the Python equivalent to the 5 sections in 
+the loops_story_clinic.Rmd file.  
 
 """
 
@@ -12,11 +12,14 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-whitegrid')
 
+# Section 1 code -----------------------------------------
 ghog_day = eval(input('Please enter the number of groundhog days: '))
 
 for idx in range(1, ghog_day + 1):
     print("When will this day end? I've already lived it", idx, "times!")
+ 
     
+# Section 2 code -----------------------------------------
 df  = pd.read_csv('c:/informatics/gapminder.csv', index_col = 0)
 
 # Set parameters for the base plot and its axes.
@@ -36,6 +39,7 @@ df_gap = df.loc['Brazil']
 plt.plot(df_gap[['year']], df_gap[['lifeExp']], color = 'green')
 
 
+# Section 3 code -----------------------------------------
 countries = ['United States','France','Brazil']
 
 # Set parameters for the base plot and its axes.
@@ -55,6 +59,7 @@ df_gap = df.loc[countries[2]]
 plt.plot(df_gap[['year']], df_gap[['lifeExp']], color = 'green')
 
 
+# Section 4 code ----------------------------------------
 countries   = ['United States','France','Brazil']
 line_colors = ['blue','green','red']
 
@@ -72,14 +77,12 @@ for idx in range(len(countries)):
 
 idx = 1
 
+
+# Section 5 code ---------------------------------------
 ghog_day = eval(input('Please enter the number of groundhog days: '))
 
 while idx < (ghog_day + 1):
      print("When will this day end? I've already lived it", idx, "times!")
      # Where is idx incremented?  without it, this is an infinite loop.
 # end while loop
-
-if __name__ == "__main__":
-    
-    df = pd.read_csv('c:/informatics/gapminder.csv')
     
