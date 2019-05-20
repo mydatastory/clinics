dffdd64252a5c04ac7105caa6c89c7fc5a32ce3a
Daniel Maxwell <dsm_direct@yahoo.com>
2019-05-20 14:51:18 -0400

    Completed base version.

--- _fish/fish_code.py
+++ _fish/fish_code.py
@@ -5,13 +5,28 @@ Created on Thu May 16 12:38:42 2019
 @author: danielmaxwell
 """
 
+# Environment setup with appropriate libraries.
 import pandas as pd
 import matplotlib.pyplot as plt
 
-base_df = pd.read_csv('c:/informatics/landings.csv')
+# Load the data into a base data frame.
+base_df = pd.read_csv('c:/informatics/cod_landings.csv')
 
-tmp_df = base_df.groupby(['year','state','species'], as_index = False)['pounds'].sum()
+# The grouping and summing operation below was necessary because the NOAA 
+# supplied data set had multiple rows for each state in 1988.
+catch_df = base_df.groupby(['year','state','species'], as_index = False)['pounds'].sum()
 
-tmp_df.columns = ['year','state','species','sum_pounds']
+# Assign column names and set state as the index.
+catch_df.columns = ['year','state','species','sum_pounds']
+catch_df = catch_df.set_index('state')
+
+# Subset the MA data.
+mass_df = catch_df.loc['Massachusetts']
+
+# Plot the MA Cod catch.
+plt.plot(mass_df.year, mass_df.sum_pounds / 2000)
+plt.title('Atlantic Cod (MA Landings)')
+plt.ylabel('Tons')
+plt.xlabel('Year')
 
 
