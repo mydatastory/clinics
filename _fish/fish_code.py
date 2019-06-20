3cddba1fac1f4cb68762e07fc9777f5062ce2a12
Daniel Maxwell <dsm_direct@yahoo.com>
2019-06-20 16:14:47 -0400

    Added plot to graph cod landings from all seven states.

--- _fish/fish_code.py
+++ _fish/fish_code.py
@@ -12,21 +12,56 @@ import matplotlib.pyplot as plt
 # Load the data into a base data frame.
 base_df = pd.read_csv('c:/informatics/cod_landings.csv')
 
-# The grouping and summing operation below was necessary because the NOAA 
-# supplied data set had multiple rows for each state in 1988.
+# Pivot the data set from long to wide format to view pounds caught for each  
+# state by year.  Compare numbers from late 1980's to present.  The grouping
+# and summing operation is necessary because the NOAA supplied data set often
+# has multiple observations for a given state in a given year.
+pbase_df = base_df.groupby(['year','state'], as_index = False).sum()
+
+pivot_df = pbase_df.pivot(index = 'state', columns = 'year', values = 'pounds')
+pivot_df.head(n = 7)
+
+# Viewing data in a table gives us an initial sense of catch sizes over time.
+# An initial inspection of the numbers suggests that there have been some
+# dramatic declines in catches from the late 1980's.  Let's plot the data for
+# one state (Massachussetts) to get a sense of what's happening. 
 cod_df = base_df.groupby(['year','state','species'], as_index = False)['pounds'].sum()
 
 # Assign column names and set state as the index.
 cod_df.columns = ['year','state','species','sum_pounds']
-cod_df = catch_df.set_index('state')
+cod_df = cod_df.set_index('state')
 
 # Subset the MA data.
-mass_df = cod_df.loc['Massachusetts']
+ma = cod_df.loc['Massachusetts']
 
-# Plot the MA Cod catch.
-plt.plot(mass_df.year, mass_df.sum_pounds / 2000)
+# Generate the MA plot.
+plt.plot(ma.year, ma.sum_pounds / 2000)
 plt.title('Atlantic Cod (MA Landings)')
 plt.ylabel('Tons')
 plt.xlabel('Year')
+
+# Plot and compare the seven states in our data set.  Do catch numbers follow
+# a similar pattern across the states?  Subset the data into dataframes, one
+# for each state.
+me = cod_df.loc['Maine']
+ct = cod_df.loc['Connecticut']
+ri = cod_df.loc['Rhode Island']
+nh = cod_df.loc['New Hampshire']
+nj = cod_df.loc['New Jersey']
+md = cod_df.loc['Maryland']
+
+# Generate the plot.  Do all of the states follow a similar pattern?
+plt.plot(ma.year, ma.sum_pounds / 2000)
+plt.plot(me.year, me.sum_pounds / 2000)
+plt.plot(ct.year, ct.sum_pounds / 2000)
+plt.plot(ri.year, ri.sum_pounds / 2000)
+plt.plot(nh.year, nh.sum_pounds / 2000)
+plt.plot(nj.year, nj.sum_pounds / 2000)
+plt.plot(md.year, md.sum_pounds / 2000)
+plt.title('Atlantic Cod (Landings)')
+plt.ylabel('Tons')
+plt.xlabel('Year')
+plt.legend(['MA','ME','CT','RI','NH','NJ','MD'])
+
 
 
