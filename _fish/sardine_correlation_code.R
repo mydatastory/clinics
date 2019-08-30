5c344e150dc5ddef804a79d3ece99d91c21a394a
Daniel Maxwell <dsm_direct@yahoo.com>
2019-08-30 15:03:50 -0400

    Cleaned code and added documentation.

--- _fish/sardine_correlation_code.R
+++ _fish/sardine_correlation_code.R
@@ -1,14 +1,28 @@
+# -------------------------------------------------------------------------------------------
+# Sometimes the 'noise' in our raw data prevents us from seeing the relationship between
+# two variables.  In this learning experience, we plot raw sardine catch and ocean temperature
+# data and then create smoothing lines for both, before calculating a Pearson's r correlation
+# between the two.  Smoothed data is preferable in this case because the raw data is noisy.
+# -------------------------------------------------------------------------------------------
+
 library(dplyr)
 
 # -------------------------------------------------------------------------------------------
-# Environment setup and load the two datasets.
+# Environment setup and load the two datasets.  The first is ocean temperature readings from
+# La Jolla, CA, the second is sardine landings (total catch) in tons.
 # -------------------------------------------------------------------------------------------
 
 temps_df    <- read.csv('scripps_ocean_temps.csv', header = TRUE, stringsAsFactors = FALSE)
 landings_df <- read.csv('sardine_landings_ueber.csv', header = TRUE, stringsAsFactors = FALSE)
 
 # -------------------------------------------------------------------------------------------
-# Because the onset of sardine spawning begins in February, create a subset of the temperature data from February to April.  This is the peak spawning season.  Then create a new data set of average temperatures for each year of the dataset. https://stackoverflow.com/questions/3480388/how-to-fit-a-smooth-curve-to-my-data-in-r  http://r-statistics.co/Loess-Regression-With-R.html
+# Because the onset of sardine spawning begins in February, create a subset of the temperature 
+# data from February to April.  This is the peak spawning season.  Then create a new data set of 
+# average temperatures for each year of the dataset. 
+# 
+# References for creating smoothing lines in R.
+# https://stackoverflow.com/questions/3480388/how-to-fit-a-smooth-curve-to-my-data-in-r  
+# http://r-statistics.co/Loess-Regression-With-R.html
 # -------------------------------------------------------------------------------------------
 
 temps_df <- temps_df[,-5]
@@ -22,73 +36,72 @@ avg_temps_df <- temps_df %>%
 
 colnames(avg_temps_df)[2] <- "avg_temp"
 
-temp_model <- loess(avg_temp ~ year, data = avg_temps_df, span = .50)
-temp_line  <- predict(temp_model)
+# -------------------------------------------------------------------------------------------
+# Plot the raw temperature data and then create a temperature model (loess), generate predicted 
+# values using that model, and then lay down a smoothing line on top of the plot.
+# -------------------------------------------------------------------------------------------
 
 plot(avg_temps_df$year, avg_temps_df$avg_temp,                  
      type = "l",
      col  = "blue",
      main = "",
      xlim = c(1915, 1970),
-     xlab = "",
-     ylab = '')
+     xlab = "Year",
+     ylab = 'Temperature (Celsius)')
+
+temp_model <- loess(avg_temp ~ year, data = avg_temps_df, span = .50)
+temp_line  <- predict(temp_model)
 
 lines(temp_line, x = avg_temps_df$year, col = 'red')
 
-landing_model <- loess(monterey_tons ~ year, data = landings_df, span = .50)
-landing_line  <- predict(landing_model)
+# -------------------------------------------------------------------------------------------
+# Plot the raw landings data and then create a landings model (loess), generate predicted 
+# values using that model, and then lay down a smoothing line on top of the plot.
+# -------------------------------------------------------------------------------------------
 
 plot(landings_df$year, landings_df$monterey_tons,                  
      type = "l",
      col  = "blue",
      main = "",
      xlim = c(1915, 1970),
-     xlab = '',
-     ylab = '')
+     xlab = 'Year',
+     ylab = 'Landings (Tons)')
+
+landing_model <- loess(monterey_tons ~ year, data = landings_df, span = .50)
+landing_line  <- predict(landing_model)
 
 lines(landing_line, x = landings_df$year, col = 'red')
 
-catch_df <- data.frame(landing_line)
-temp_df  <- data.frame(temp_line)
+# -------------------------------------------------------------------------------------------
+# With the smoothing line vectors, create two new dataframes.  Add a year variable, populate
+# it with values from 1917 to 1955, and then join both into an analysis dataframe.  Remove rows
+# from analysis_df for years when the fishery was shut down.  Plot the new dataset.  Note:
+# the landing and temp dataframes are singular to distinguish them from the original (plural) 
+# dataframes created earlier.
+# -------------------------------------------------------------------------------------------
 
-catch_df$year <- 1917:1967
-temp_df$year  <- 1917:1967
+landing_df <- data.frame("landings" = landing_line)
+temp_df    <- data.frame("temps" = temp_line)
 
-analysis_df <- merge(temp_df, catch_df, by = 'year')
-analysis_df <- subset(analysis_df, landing_line > 500)
+landing_df$year <- 1917:1967
+temp_df$year    <- 1917:1967
+
+analysis_df <- merge(temp_df, landing_df, by = 'year')
+analysis_df <- subset(analysis_df, landings > 500)
 analysis_df <- subset(analysis_df, year < 1954)
 
-plot(x = analysis_df$temp_line, y = analysis_df$landing_line,
+plot(x = analysis_df$temps, y = analysis_df$landings,
      xlab = 'Surface Temp (Celsius)',
      ylab = 'Tons')
 
-fit <- lm(analysis_df$landing_line ~ analysis_df$temp_line)
+# -------------------------------------------------------------------------------------------
+# Generate a simple linear regression model, add the regression line to the plot and then
+# calculate Pearson's r between the smoothed temperatures and the smoothed landings.
+# -------------------------------------------------------------------------------------------
+
+fit <- lm(analysis_df$landings ~ analysis_df$temps)
 
 abline(fit, col = 'red')
 
-cor(analysis_df$landing_line, analysis_df$temp_line)
+cor(analysis_df$landings, analysis_df$temps)
 
-# -------------------------------------------------------------------------------------------
-# Join the two datasets on year and retain only those years where the catch was greater than 500 tons.  When the catch was less than 500 tons, the fishery was effectively shutdown.
-# -------------------------------------------------------------------------------------------
-
-analysis_df <- merge(temps_df, landings_df, by = 'year')
-analysis_df <- subset(analysis_df, monterey_tons > 500)
-
-# -------------------------------------------------------------------------------------------
-# Create a simple linear model with landings as the response variable surface temperature as the predictor variable.
-# -------------------------------------------------------------------------------------------
-
-fit <- lm(analysis_df$monterey_tons ~ analysis_df$surf_temp_c)
-
-# Create a scatterplot of the data and overlay it with the regression line.
-
-plot(x = analysis_df$surf_temp_c, y = analysis_df$monterey_tons,
-     xlab = 'Surface Temp (Celsius)',
-     ylab = 'Tons')
-
-abline(fit, col = 'red')
-
-# Calculate the correlation (Pearson's r) between landings and surface temperature.
-
-cor(analysis_df$monterey_tons, analysis_df$surf_temp_c)
