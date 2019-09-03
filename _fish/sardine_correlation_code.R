eca84045a80097a19e3ce7007549e89f75ed6ff6
Daniel Maxwell <dsm_direct@yahoo.com>
2019-09-03 12:07:24 -0400

    Added regression and correlation code for the raw data.

--- _fish/sardine_correlation_code.R
+++ _fish/sardine_correlation_code.R
@@ -17,8 +17,8 @@ landings_df <- read.csv('sardine_landings_ueber.csv', header = TRUE, stringsAsFa
 
 # -------------------------------------------------------------------------------------------
 # Because the onset of sardine spawning begins in February, create a subset of the temperature 
-# data from February to April.  This is the peak spawning season.  Then create a new data set of 
-# average temperatures for each year of the dataset. 
+# data from February to April.  This is the peak spawning season.  Then create a new data set 
+# of average temperatures for each year of the dataset. 
 # 
 # References for creating smoothing lines in R.
 # https://stackoverflow.com/questions/3480388/how-to-fit-a-smooth-curve-to-my-data-in-r  
@@ -73,11 +73,49 @@ landing_line  <- predict(landing_model)
 lines(landing_line, x = landings_df$year, col = 'red')
 
 # -------------------------------------------------------------------------------------------
-# With the smoothing line vectors, create two new dataframes.  Add a year variable, populate
-# it with values from 1917 to 1955, and then join both into an analysis dataframe.  Remove rows
-# from analysis_df for years when the fishery was shut down.  Plot the new dataset.  Note:
-# the landing and temp dataframes are singular to distinguish them from the original (plural) 
-# dataframes created earlier.
+# RAW TEMPERATURE AND LANDING REGRESSION & CORRELATION
+#
+# First compute a simple linear regression and Pearson's correlation on the raw data and
+# then we'll do the same with the smoothed data and compare the two.
+#
+# Data Note:  landings_df and temps_df contain raw data.  
+# -------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------
+# Join the two datasets on year and retain only those years where the catch was greater than 
+# 500 tons.  When the catch was less than 500 tons, the fishery was effectively shutdown. And
+# then plot the data.
+# -------------------------------------------------------------------------------------------
+
+analysis_df <- merge(avg_temps_df, landings_df, by = 'year')
+analysis_df <- subset(analysis_df, monterey_tons > 500)
+analysis_df <- subset(analysis_df, year < 1954)
+
+plot(x = analysis_df$avg_temp, y = analysis_df$monterey_tons,
+     xlab = 'Surface Temp (Celsius)',
+     ylab = 'Tons')
+
+# -------------------------------------------------------------------------------------------
+# Generate a simple linear regression model, add the regression line to the plot and then
+# calculate Pearson's r between the raw temperatures and the raw landings.
+# -------------------------------------------------------------------------------------------
+
+fit <- lm(analysis_df$monterey_tons ~ analysis_df$avg_temp)
+
+abline(fit, col = 'red')
+
+raw_cor <- cor(analysis_df$monterey_tons, analysis_df$avg_temp)
+
+# -------------------------------------------------------------------------------------------
+# SMOOTHED TEMPERATURE AND LANDING REGRESSION & CORRELATION
+#
+# Now that we've conducted a simple linear regression and correlation with the raw data, let's
+# do the same with the smoothed data from our loess model.  With the smoothing line vectors,
+# create two new dataframes.  Add a year variable, populate it with values from 1917 to
+# 1955, and then join both into an analysis dataframe.  Remove rows from analysis_df for years
+# when the fishery was shut down.  Plot the new dataset.  
+#
+# Data Note:  landing_df and temp_df contain smoothed data.  
 # -------------------------------------------------------------------------------------------
 
 landing_df <- data.frame("landings" = landing_line)
@@ -103,5 +141,14 @@ fit <- lm(analysis_df$landings ~ analysis_df$temps)
 
 abline(fit, col = 'red')
 
-cor(analysis_df$landings, analysis_df$temps)
+smooth_cor <- cor(analysis_df$landings, analysis_df$temps)
+
+# -------------------------------------------------------------------------------------------
+# And finally, let's compare the correlation between the raw and smoothed data.
+# -------------------------------------------------------------------------------------------
+
+raw_cor
+smooth_cor
+
+
 
